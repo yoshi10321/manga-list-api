86a33035de5e8c6dad4967bbba4e4e586a083638
yoshi10321 <pooh.aflac@gmail.com>
2017-03-18 16:38:06 +0900

    add manga model cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Manga.delete_all
+Manga.create(title: '進撃の巨人', readNumber: 1, img: 'http://bkmkn.s3-website-ap-northeast-1.amazonaws.com/9784063842760/9784063842760_w.jpg')
+Manga.create(title: 'キングダム', readNumber: 45, img: 'https://i2.wp.com/kingdom-chuukatouitsu.com/wp-content/uploads/2017/02/C1uOfsHUAAAWRjm.jpg')
+Manga.create(title: '宇宙兄弟', readNumber: 20, img: 'https://images-na.ssl-images-amazon.com/images/I/51To-5J53pL._SX350_BO1,204,203,200_.jpg') 
